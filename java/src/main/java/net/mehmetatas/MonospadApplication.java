d9c8396370a31ca04ee071a25d013e84a3398d6f
Mehmet Ataş <mehmetatasm@gmail.com>
2016-04-28 19:44:55 +0300

    added java project

---
+++ java/src/main/java/net/mehmetatas/MonospadApplication.java
@@ -0,0 +1,12 @@
+package net.mehmetatas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonospadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonospadApplication.class, args);
+	}
+}
